b86716f5d205eb43858942c62b58780f3001bf86
Rafael26Utah <rtekassala@gmail.com>
2025-09-19 21:09:02 +0200

    Update waku_score.py

--- waku_score.py
+++ waku_score.py
@@ -1,8 +1,1 @@
-import streamlit as st
-
-# Mostra o logo do Waku Score no topo
-st.image("images/waku_score_logo.jpg", use_column_width=True)
-
-# Título e subtítulo
-st.title("⚽ Aplicativo Waku - Teste ⚽")
-st.subheader("Experimente a versão inicial do Waku Score") 
+st.image("https://raw.githubusercontent.com/Rafael26Utah/Waku-App/main/images/waku_score_logo.jpg", use_column_width=True)
