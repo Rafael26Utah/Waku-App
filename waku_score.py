b5eb00d1156a85d124818c04b35ef70143eac3f8
Rafael26Utah <rtekassala@gmail.com>
2025-09-19 21:17:57 +0200

    Update waku_score.py

--- waku_score.py
+++ waku_score.py
@@ -1,1 +1,6 @@
-st.image("https://raw.githubusercontent.com/Rafael26Utah/Waku-App/main/images/waku_score_logo.jpg", use_container_width=True)
+import streamlit as st
+
+st.image(
+    "https://raw.githubusercontent.com/Rafael26Utah/Waku-App/main/images/waku_score_logo.jpg",
+    use_container_width=True
+)
