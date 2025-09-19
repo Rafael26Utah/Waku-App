2e70da24f8452aa100f8a352094008274c1d06ea
Rafael26Utah <rtekassala@gmail.com>
2025-09-19 19:43:58 +0200

    Update waku_score.py

--- waku_score.py
+++ waku_score.py
@@ -1,1 +1,11 @@
+import streamlit as st
 
+# Título do aplicativo
+st.title("Aplicativo Waku - Teste")
+
+# Entrada de texto
+nome = st.text_input("Digite o seu nome:")
+
+# Botão
+if st.button("Enviar"):
+    st.write(f"Olá, {nome}! 👋 Bem-vindo ao Waku.") 
